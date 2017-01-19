898a679a805cb243ab328a19bb5fefef17c8ab9b
andrewdc92 <andrew.cordivari@gmail.com>
2017-01-18 20:19:29 -0800

    adds posts new, posts index

--- src/reducers/reducer-posts.js
+++ src/reducers/reducer-posts.js
@@ -5,7 +5,8 @@ const INITIAL_STATE = { all: [], post: null };
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
     case FETCH_POSTS:
-
+    return { ...state, all: action.payload.data };
+    //this is the api response, ...signigies 'whatever was already here'
     default:
     return state;
   }
