b29ff90a286882d2026da537fdce00a7a0be9e85
andrewdc92 <andrew.cordivari@gmail.com>
2017-02-01 12:36:48 -0800

    refactors posts-new with lodash methods and more redux form helper methods

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,7 +1,24 @@
+import _ from 'lodash';
 import React, { Component, PropTypes} from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
+
+const FIELDS = {
+  title: {
+    type: 'input',
+    label: 'Title for Post'
+  },
+  categories: {
+    type: 'input',
+    label: 'Enter a few categories for this post'
+  },
+  content: {
+    type: 'textarea',
+    label: 'Post Contents'
+  }
+};
+// '['title', 'categories', 'content']'
 
 class PostsNew extends Component {
   static contextTypes = {
@@ -18,37 +35,27 @@ class PostsNew extends Component {
     //creats promise as a payload, when it's resolved, it means blog post was sucesfuuly created
   }
 
+  renderField(fieldConfig, field) {
+    const fieldHelper = this.props.fields[field];
+
+    return (
+      <div className={`form-group ${fieldHelper.touched && fieldHelper.invalid ? 'has-danger' : ''}`}>
+        <label>{fieldConfig.label}</label>
+        <fieldConfig.type type="text" className="form-control" {...fieldHelper} />
+        <div className="text-help">
+          {fieldHelper.touched ? fieldHelper.error : ''}
+        </div>
+      </div>
+    );
+  }
+
   render() {
-    const { fields: { title, categories, content}, handleSubmit } = this.props;
+    const { handleSubmit } = this.props;
     return (
       // form needs an action creator to receive the properties off of the form
       <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
       <h3> Create a New Post</h3>
-
-      <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
-        <label>Title</label>
-        <input type="text" className="form-control" {...title} />
-        <div className="text-help">
-          {title.touched ? title.error : ''}
-        </div>
-      </div>
-
-      <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
-        <label>Categories</label>
-        <input type="text" className="form-control" {...categories}/>
-        <div className="text-help">
-          {categories.touched ? title.error : ''}
-        </div>
-      </div>
-
-      <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
-        <label>Content</label>
-        <textarea className="form-control" {...content} />
-        <div className="text-help">
-          {content.touched ? title.error : ''}
-        </div>
-      </div>
-
+      {_.map(FIELDS, this.renderField.bind(this))}
       <button type="submit" className="btn btn-primary">Submit</button>
       <Link to="/" className="btn btn-danger">Cancel</Link>
     </form>
@@ -58,23 +65,20 @@ class PostsNew extends Component {
 
 function validate(values) {
   const errors = {};
-    if (!values.title) {
-      errors.title = "Enter a username";
+  _.each(FIELDS, (type, field) => {
+    if (!values[field]) {
+      errors[field] = `Enter ${field}`;
     }
-    if (!values.categories) {
-      errors.categories = 'Enter categories';
-    }
-    if (!values.content) {
-      errors.content = 'Enter some content';
-    }
+  });
+
   return errors;
 }
 //connect: 1st argument is mapStateToProps, 2nd is mapDispatchToProps
 // reduxForm, 1st is form config, 2nd is mapStateToProps, 3rd, is mapDispatchToProps
 export default reduxForm({
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content'],
+  fields: _.keys(FIELDS),
   validate
-}, null, { createPost })(PostsNew);
+})(PostsNew);
 //redux form can be used just like connect to inject action creators into component, and create a container
 // out of the component.
